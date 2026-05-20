c733b6b2aaa9e02238b3a46f67ac8c134aca3c2a
Denis <someemail@email.ru>
2026-05-20 18:07:48 +0000

    3

---
+++ 3/main3.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	ch := make(chan bool)
+	timer := time.NewTimer(2*time.Second)
+	go func() {
+		time.Sleep(5*time.Second)
+		ch <- true
+	}()
+
+	select {
+	case <- ch:
+		fmt.Println("operation done")
+	//case <-time.After(2*time.Second):
+	//	fmt.Println("timeout!")
+	case <- timer.C:
+		fmt.Println("time is gone!")
+	}
+}
+
